6d51e4044ccce55b025c0360644410809044c8e6
KentaroKobaZscaler <64397608+KentaroKobaZscaler@users.noreply.github.com>
2022-09-03 17:29:54 +0900

    Update main.tf

--- beginners/aws/lab01-terraform/main.tf
+++ beginners/aws/lab01-terraform/main.tf
@@ -1,17 +1,17 @@
 terraform {
-    required_version = ">= 1.0.0"
+#    required_version = ">= 1.0.0"
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = "~> 3.37"
+ #           version = "~> 3.37"
         }
         local = {
             source = "hashicorp/local"
-            version = "2.1.0"
+ #           version = "2.1.0"
         }
         tls = {
             source = "hashicorp/tls"
-            version = "3.1.0"
+  #          version = "3.1.0"
         }
     }
 }
