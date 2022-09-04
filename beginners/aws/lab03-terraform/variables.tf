6a3cb424c4ce8bf4aee25d1d4938e557a79ed922
KentaroKobaZscaler <64397608+KentaroKobaZscaler@users.noreply.github.com>
2022-09-04 10:17:35 +0900

    Create variables.tf

---
+++ beginners/aws/lab03-terraform/variables.tf
@@ -0,0 +1,33 @@
+# Set your AWS Region
+variable "aws_region" {
+   description = "AWS Region to launch servers"
+   default = "ap-southeast-1"
+}
+
+
+variable "aws_amis" {
+   default = {
+       ap-southeast-1 = "ami-02ee763250491e04a" # Ubuntu
+      # ca-central-1 = "ami-098e42ae54c764c35"
+   }
+}
+
+variable "instance_type" {
+   description = "Type of AWS EC2 instance."
+   default     = "t2.micro"
+}
+
+variable "instance_count" {
+   default = 1
+}
+
+variable "public_key_path" {
+   description = "Enter the path to the SSH Public Key to add to AWS."
+   default     = "SKOFY23AWSKey.pem"
+}
+
+variable "key_name" {
+   description = "AWS Key Name"
+   default     = "SKOFY23AWSKey"
+}
+
